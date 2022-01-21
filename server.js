cdf0441b7c4a1245f73ac1a8380ab6778d0fe16b
sirteddanson <knowledgeMED@gmail.com>
2022-01-21 16:00:46 -0500

    post/get to front end

--- server.js
+++ server.js
@@ -1,46 +1,43 @@
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
-const notes  = require('./Develop/db/db.json');
+const notes  = require('./db/db.json');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get('/api/notes', (req, res) => {
-  res.json(notes);
-});
-
-app.get('/notes', (req, res) => {
-  res.json(notes);
-});
-
-function findById(id, notes) {
-  const result = notes.filter(note => note.id === id)[0];
-  return result;
-};
+// function findById(id, notes) {
+//   const result = notes.filter(note => note.id === id)[0];
+//   return result;
+// };
 
 function createNewNote(body, notes) {
   const note = body;
   notes.push(note);
   fs.writeFileSync(
-    path.join(__dirname, './Develop/db/db.json'),
+    path.join(__dirname, './db/db.json'),
     JSON.stringify(notes, null, 2)
   );
 
   return note;
-}
+};
 
-app.get('/api/notes/:id', (req, res) => {
-  const result = findById(req.params.id, notes);
-  if (result) {
-    res.json(result);
-  } else {
-    res.send(404);
-  }
+app.get('/api/notes', (req, res) => {
+  res.json(notes);
 });
+
+// app.get('/api/notes/:id', (req, res) => {
+//   const result = findById(req.params.id, notes);
+//   if (result) {
+//     res.json(result);
+//   } else {
+//     res.send(404);
+//   }
+// });
 
 app.post('/api/notes', (req, res) => {
   req.body.id = notes.length.toString();
@@ -50,6 +47,17 @@ app.post('/api/notes', (req, res) => {
   res.json(note);
 });
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
